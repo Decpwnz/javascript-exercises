92bdf3f64120f964d0bd9152aa240c22a6f18489
Marijus Ladanovski <electromenn@gmail.com>
2025-05-01 11:21:18 +0300

    Complete excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // 1. split string into array
+  return (
+    string
+      .split("")
+      // 2. reverse array
+      .reverse()
+      // 3. join back array into string
+      .join("")
+  );
 };
 
 // Do not edit below this line
