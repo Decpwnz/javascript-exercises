3d6c4759792cc5f43eb28dc55a92e2333cec3995
Marijus Ladanovski <electromenn@gmail.com>
2025-05-01 11:15:10 +0300

    Complete 02 excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let init = "";
 
+  if (num < 0) return "ERROR";
+
+  for (let i = 0; i < num; i++) {
+    init = init + string;
+  }
+  return init;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
