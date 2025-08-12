7f04817a8b62aca51747a26e5210d98ca6424de8
Marijus Ladanovski <electromenn@gmail.com>
2025-08-12 10:40:12 +0300

    Refactor reverseString function and update start script path

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,8 @@
-const reverseString = function (string) {
-  // 1. split string into array
-  return (
-    string
-      .split("")
-      // 2. reverse array
-      .reverse()
-      // 3. join back array into string
-      .join("")
-  );
-};
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
