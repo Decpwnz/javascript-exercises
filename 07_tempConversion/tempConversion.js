9e0175c745c94dc6475b6bae5acac76e1b83b5eb
Marijuš Ladanovski <electromenn@gmail.com>
2025-12-03 20:03:06 +0200

    Refactor temperature conversion functions and update test cases for consistency

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value) {
+  const convertedDegrees = (value - 32) * (5 / 9);
+  const roundedResult = Math.round(convertedDegrees * 10) / 10;
+
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (value) {
+  convertDegrees = Math.round((value * (9 / 5) + 32) * 10) / 10;
+
+  return convertDegrees;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
