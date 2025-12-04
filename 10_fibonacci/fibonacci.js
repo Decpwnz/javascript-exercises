5baf4016e76f98923ddf4024a1a4fb3a9e7dbff7
Marijuš Ladanovski <electromenn@gmail.com>
2025-12-04 22:10:57 +0200

    Refactor fibonacci function to accept a number parameter and update test cases for consistency

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let num;
+  const arr = [0, 1];
 
+  if (number < 0 && !isNaN(number)) return "OOPS";
+
+  for (let i = 0; i < number; i++) {
+    num = arr[arr.length - 1] + arr[arr.length - 2];
+    arr.push(num);
+  }
+  const finalIndex = arr[arr.length - 2];
+
+  return finalIndex;
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
