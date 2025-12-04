75ee803b20eb1a17eb8b24a12aefdfe6c259156c
Marijuš Ladanovski <electromenn@gmail.com>
2025-12-04 13:58:33 +0200

    Refactor palindromes function to accept a string parameter and update test cases for consistency

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const punctuation = /[\.,?!]/g;
+  const newStr = string
+    .replace(punctuation, "")
+    .toLowerCase()
+    .split(" ")
+    .join("");
+  console.log("newStr", newStr);
 
+  const reversedString = newStr.split("").reverse().join("");
+  return newStr === reversedString;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
