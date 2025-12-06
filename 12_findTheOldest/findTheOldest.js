b3280f71c8082c9a100bcac4d4e1fba7eee4300f
Marijuš Ladanovski <electromenn@gmail.com>
2025-12-06 23:17:44 +0200

    Refactor findTheOldest function to accept an array parameter and update test cases for consistency

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,66 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  // const newArr = arr
+  //   .map((element, index, array) => {
+  //     if (
+  //       !element.yearOfBirth ||
+  //       !element.yearOfDeath ||
+  //       element.yearOfDeath === "undefined"
+  //     )
+  //       return;
+  //     const age = element.yearOfDeath - element.yearOfBirth;
+  //     return { ...element, age };
+  //   })
+  //   .sort((a, b) => b.age - a.age);
+  // return newArr[0];
 
+  const getOldest = arr.reduce((a, b) => {
+    if (!a.yearOfDeath || !b.yearOfDeath) {
+      return a.yearOfBirth < b.yearOfBirth ? a : b;
+    } else {
+      return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth
+        ? a
+        : b;
+    }
+  }, 0);
+
+  return getOldest;
 };
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
